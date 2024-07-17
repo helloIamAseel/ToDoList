d345a1bab7c04bf46fc83a0a39021ecc84f337c8
Aseel <aseel4leesa@gmail.com>
2024-07-17 17:07:41 +0300

    added pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import '../app.postcss';
-
+	//@ts-ignore
+    import { pwaInfo } from 'virtual:pwa-info'; 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
     import { initializeStores, Modal } from '@skeletonlabs/skeleton';
     initializeStores();
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
 </script>
+
+<svelte:head> 
+ 	{@html webManifestLink} 
+</svelte:head>
 
 <Modal buttonPositive="bg-error-500"
        buttonNeutral="bg-gray-300" />
